2fc275d2e63fe5f4bbfe9b541af28c0ec81825a8
Mark Gukov <markgukov@gmail.com>
2018-04-20 16:08:25 -0700

    Updated the regex in parseDocument to be OK with no double slashes as they are removed now

--- pkg/emd/parseDocument.go
+++ pkg/emd/parseDocument.go
@@ -6,31 +6,27 @@ import (
 )
 
 func parseDocument(emdInput string, lineItems []Item) []Item {
-	if strings.Contains(emdInput, "//") {
-		re, err := regexp.Compile("^(.*) *\\* *\\/\\/ *(.*)")
-		if err != nil {
-			panic(err)
-		}
-		document := re.FindAllStringSubmatch(emdInput, -1)
-		if len(document) > 0 {
-			var properties []Property
-			first := document[0]
-			propertiesList := first[2]
-			propertiesList = strings.Trim(propertiesList, ", ")
-			inputProperties := strings.Split(propertiesList, ",")
-			for _, inputParameter := range inputProperties {
-				if inputParameter != "" {
-					var parsedParameter = Property{Name: strings.Trim(inputParameter, " ")}
-					properties = append(properties, parsedParameter)
-				}
-			}
-			lineItems = append(lineItems, Document{Name: strings.Trim(document[0][1], " "), Properties: properties})
-			return lineItems
-		}
-	} else {
-		spacesRemoved := strings.Trim(emdInput, " ")
-		documentName := strings.Replace(spacesRemoved, "*", "", -1)
-		lineItems = append(lineItems, Document{Name: documentName})
+	re, err := regexp.Compile("^(.*) *\\* (.*)")
+	if err != nil {
+		panic(err)
 	}
+	emdInput = strings.Replace(emdInput, "//", "", -1)
+	document := re.FindAllStringSubmatch(emdInput, -1)
+	if len(document) > 0 {
+		var properties []Property
+		first := document[0]
+		propertiesList := first[2]
+		propertiesList = strings.Trim(propertiesList, ", ")
+		inputProperties := strings.Split(propertiesList, ",")
+		for _, inputParameter := range inputProperties {
+			if inputParameter != "" {
+				var parsedParameter = Property{Name: strings.Trim(inputParameter, " ")}
+				properties = append(properties, parsedParameter)
+			}
+		}
+		lineItems = append(lineItems, Document{Name: strings.Trim(document[0][1], " "), Properties: properties})
+		return lineItems
+	}
+
 	return lineItems
 }
