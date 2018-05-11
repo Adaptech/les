e772fa86fa5ba35f76d99c864a3569b52c1477ac
Robert Reppel <robert.reppel@adaptechsolutions.ca>
2018-05-10 17:29:08 -0700

    UX: EMD 'command -> parameter1, parameter2' is now valid.

--- pkg/emd/parseCommand_test.go
+++ pkg/emd/parseCommand_test.go
@@ -26,6 +26,27 @@ func TestShouldGetCommandWithParameters(t *testing.T) {
 	}
 }
 
+func Test_should_get_command_with_parameters_without_slashes(t *testing.T) {
+	input := []string{"  Validate Registration  -> userId                     "}
+	result, err := emd.Parse(input)
+	if err != nil {
+		panic(err)
+	}
+	if len(result.Lines) == 0 {
+		t.Error("no command found")
+		return
+	}
+	switch result.Lines[0].(type) {
+	case emd.Command:
+		if result.Lines[0].(emd.Command).Name != "Validate Registration" {
+			t.Log(result)
+			t.Error("Unexpected Command.Name")
+		}
+	default:
+		t.Error("expected command")
+	}
+}
+
 func TestShouldFindCommandParameters(t *testing.T) {
 	input := []string{"Validate->// userId  ,  "}
 	result, err := emd.Parse(input)
@@ -44,6 +65,7 @@ func TestShouldFindCommandParameters(t *testing.T) {
 	case emd.Command:
 		parameters := result.Lines[0].(emd.Command).Parameters
 		if len(parameters) != 1 {
+			t.Log(parameters)
 			t.Error("Unexpected number of command.Parameters")
 		}
 		if parameters[0].Name != "userId" {
