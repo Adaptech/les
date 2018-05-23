5b15074a5090d498d983d8ff266c838179750ce9
Nicolas Dextraze <nicolas@adaptechsolutions.ca>
2018-05-23 12:26:07 -0700

    les-node looks for latest release on github

--- cmd/les-node/buildCommand.go
+++ cmd/les-node/buildCommand.go
@@ -14,21 +14,83 @@ import (
 	"github.com/Adaptech/les/pkg/eml/generate"
 	"github.com/Adaptech/les/pkg/utils"
 	"github.com/ghodss/yaml"
+	"encoding/json"
+	"strings"
 )
 
-const latestTemplateName = "les-node-template-20180504-2d42cce"
-const nodejsTemplateDownloadURL = "https://github.com/Adaptech/les/raw/master/releases/les-node-template/" + latestTemplateName + ".tar.gz"
+const latestReleaseUrl = "https://api.github.com/repos/Adaptech/les-node-template/releases/latest"
+
+func getLatestReleaseInfoFromGithub() (tagName string, browserDownloadUrl string) {
+	tagName = ""
+	browserDownloadUrl = ""
+
+	metadata := map[string]interface{} {}
+	if res, err := http.Get(latestReleaseUrl); err != nil {
+		log.Printf("Error downloading latest release metadata: %s", err.Error())
+		return
+	} else if jsonMetadata, err := ioutil.ReadAll(res.Body); err != nil {
+		log.Printf("Error reading latest release metadata: %s", err.Error())
+		return
+	} else if err := json.Unmarshal(jsonMetadata, &metadata); err != nil {
+		log.Printf("Error parsing latest release metadata: %s", err.Error())
+		return
+	}
+
+	var ok bool
+	tagName, ok = metadata["tag_name"].(string)
+	if !ok {
+		log.Println("Error parsing latest release metadata: tag_name is not a string.")
+		return
+	}
+
+	assets, ok := metadata["assets"].([]interface{})
+	if !ok {
+		log.Println("Error parsing latest release metadata: assets is not an array.")
+		return
+	}
+	if len(assets) < 1 {
+		log.Println("Error parsing latest release metadata: assets is empty.")
+		return
+	}
+	asset, ok := assets[0].(map[string]interface{})
+	if !ok {
+		log.Println("Error parsing latest release metadata: asset[0] is not an object.")
+	}
+	browserDownloadUrl, ok = asset["browser_download_url"].(string)
+	if !ok {
+		log.Println("Error parsing latest release metadata: browser_download_url is not a string.")
+		return
+	}
+
+	return
+}
 
 func ensureTemplateExists(templateDir string, lang string) {
 	langTemplateDir := path.Join(templateDir, lang)
+
+	fmt.Println("Checking for latest template version on github...")
+	tagName, latestTemplateUrl := getLatestReleaseInfoFromGithub()
+
+	var latestTemplateName string
+	if tagName == "" || latestTemplateUrl == "" {
+		fmt.Println("Looking for latest template version locally...")
+		latestTemplateNameBytes, err := ioutil.ReadFile(path.Join(langTemplateDir, ".latest"))
+		if err != nil {
+			log.Fatalf("Error reading local latest metadata: %s", err.Error())
+		}
+		latestTemplateName = string(latestTemplateNameBytes)
+	} else {
+		latestTemplateName = strings.Replace(tagName, "release-", "les-node-template-", 1)
+	}
+	fmt.Println("Latest template version:", latestTemplateName)
+
 	latestTemplateDir := path.Join(langTemplateDir, latestTemplateName)
 	if _, err := os.Stat(latestTemplateDir); err == nil {
 		return
 	}
 
-	var templateURL = nodejsTemplateDownloadURL
-	fmt.Println(lang, "template missing locally, downloading from server...")
-	if res, err := http.Get(templateURL); err != nil {
+	fmt.Println(lang, "template", latestTemplateName, "missing locally, downloading from server...")
+	if res, err := http.Get(latestTemplateUrl); err != nil {
 		log.Fatalf("Error downloading template: %s", err.Error())
 	} else if err := utils.Untar(langTemplateDir, res.Body); err != nil {
 		log.Fatalf("Error extracting archive: %s", err.Error())
